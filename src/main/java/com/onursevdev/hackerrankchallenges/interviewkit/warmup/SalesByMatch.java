ee0c33c7c8b273be2e2046ef0aae3f50ee9cdac6
Onur Ozdemir <onursevket44@gmail.com>
2021-09-16 23:46:47 +0300

    The "SalesByMatch" problem is solved

--- src/main/java/com/onursevdev/hackerrankchallenges/interviewkit/warmup/SalesByMatch.java
+++ src/main/java/com/onursevdev/hackerrankchallenges/interviewkit/warmup/SalesByMatch.java
@@ -5,24 +5,21 @@ import java.util.Arrays;
 import java.util.List;
 
 public class SalesByMatch {
-    public int getSocksPairCount(int singleSocksCount, List<Integer> singleSocksArray) {
-        int sockCount = 0;
+    public int getSocksPairCount(int singleSocksCount, List<Integer> singleSocksList) {
+        int pairSocks = 0;
 
-        ArrayList<Integer> arrayList = new ArrayList<>(singleSocksArray);
+        ArrayList<Integer> arrayList = new ArrayList<>(singleSocksList);
 
         for (int i = 0; i < arrayList.size(); i++) {
-            for (int j = 1; j < arrayList.size(); j++) {
-                outerLoop:
-                if (i <= singleSocksCount && arrayList.get(i) == arrayList.get(j)) {
-                    sockCount++;
-                    arrayList.remove(i);
+            for (int j = i + 1; j < arrayList.size(); j++) {
+                if (arrayList.get(i) == arrayList.get(j)) {
+                    pairSocks++;
                     arrayList.remove(j);
-                    i = 0;
-                    j = 0;
-                    break outerLoop;
+                    arrayList.remove(i);
+                    i = j = 0;
                 }
             }
         }
-        return sockCount;
+        return pairSocks;
     }
 }
--- src/test/java/com/onursevdev/hackerrankchallenges/interviewkit/warmup/SalesByMatchTest.java
+++ src/test/java/com/onursevdev/hackerrankchallenges/interviewkit/warmup/SalesByMatchTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -23,24 +24,33 @@ public class SalesByMatchTest {
     private SalesByMatch salesByMatch;
 
     @Before
-    public void before() throws Exception {
+    public void before() {
         salesByMatch = new SalesByMatch();
-    }
-
-    @After
-    public void after() throws Exception {
     }
 
     @Test
     public void SalesByMatch_GetSocksCount() {
 
-        List<Integer> singleSocksArray = Arrays.asList(10, 20, 20, 10, 10, 30, 50, 10, 20);
-        int singleSocksCount = 9;
+        List<Integer> oddSocksArraySample1 = Arrays.asList(10, 20, 20, 10, 10, 30, 50, 10, 20);
+        int singleSocksCountSample1 = 9;
 
+        List<Integer> oddSocksArraySample2 = Arrays.asList(1, 1, 3, 1, 2, 1, 3, 3, 3, 3);
+        int singleSocksCountSample2 = 10;
 
-        int result=salesByMatch.getSocksPairCount(singleSocksCount,singleSocksArray);
+        List<Integer> oddSocksArraySample3 = Arrays.asList(42 ,42 ,42 ,42 ,42 ,42 ,42 ,42 ,42, 42);
+        int singleSocksCountSample3 = 10;
 
-        Assert.assertEquals(result,3);
+        int result1 = salesByMatch.getSocksPairCount(singleSocksCountSample1, oddSocksArraySample1);
+
+        Assert.assertEquals(result1, 3);
+
+        int result2 = salesByMatch.getSocksPairCount(singleSocksCountSample2, oddSocksArraySample2);
+
+        Assert.assertEquals(result2, 4);
+
+        int result3 = salesByMatch.getSocksPairCount(singleSocksCountSample3, oddSocksArraySample3);
+
+        Assert.assertEquals(5,result3 );
     }
 
 } 
